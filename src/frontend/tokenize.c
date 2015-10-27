698a348b2ad8bbbe6f08029780df5f2c0f2b0e92
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2015-10-27 22:31:47 +0100

    Tokenize by branching instead of string compare

--- src/frontend/tokenize.c
+++ src/frontend/tokenize.c
@@ -15,7 +15,7 @@
 /* Parse identifier. The C standard specifies a fixed lower limit on the
  * length, which makes it possible to store identifiers in a static buffer.
  */
-static const char *strtoident(char *in, char **endptr)
+/*static const char *strtoident(char *in, char **endptr)
 {
     static char identifier[64];
     char c, *str;
@@ -37,7 +37,7 @@ static const char *strtoident(char *in, char **endptr)
     }
 
     return identifier;
-}
+}*/
 
 /* Parse integer literal in the format '1234', '0x123', '077' using strtol,
  * then skip any type suffix (uUlL). The type is discarded.
@@ -157,91 +157,212 @@ static void strtospace(char *in, char **endptr)
     *endptr = in;
 }
 
-static struct token keywords[] = {
-    { AUTO, "auto" },
-    { BREAK, "break" },
-    { CASE, "case" },
-    { CHAR, "char" },
-    { CONST, "const" },
-    { CONTINUE, "continue" },
-    { DEFAULT, "default" },
-    { DOUBLE, "double" },
-    { DO, "do" },
-    { ELSE, "else" },
-    { ENUM, "enum" },
-    { EXTERN, "extern" },
-    { FLOAT, "float" },
-    { FOR, "for" },
-    { GOTO, "goto" },
-    { IF, "if" },
-    { INT, "int" },
-    { LONG, "long" },
-    { REGISTER, "register" },
-    { RETURN, "return" },
-    { SHORT, "short" },
-    { SIGNED, "signed" },
-    { SIZEOF, "sizeof" },
-    { STATIC, "static" },
-    { STRUCT, "struct" },
-    { SWITCH, "switch" },
-    { TYPEDEF, "typedef" },
-    { UNION, "union" },
-    { UNSIGNED, "unsigned" },
-    { VOID, "void" },
-    { VOLATILE, "volatile" },
-    { WHILE, "while" }
-};
+/* Valid identifier character, except in the first position which does not
+ * allow numbers.
+ */
+#define isident(c) (isalnum(c) || (c) == '_')
 
-static struct token operators[] = {
-    { MUL_ASSIGN, "*=", },
-    { DIV_ASSIGN, "/=", },
-    { MOD_ASSIGN, "\x25=" },
-    { PLUS_ASSIGN, "+=" },
-    { MINUS_ASSIGN, "-=" },
-    { LSHIFT_ASSIGN, "<<=" },
-    { RSHIFT_ASSIGN, ">>=" },
-    { AND_ASSIGN, "&=" },
-    { XOR_ASSIGN, "^=" },
-    { OR_ASSIGN, "|=" },
-    { DOTS, "..." },
-    { LOGICAL_OR, "||" },
-    { LOGICAL_AND, "&&" },
-    { LEQ, "<=" },
-    { GEQ, ">=" },
-    { EQ, "==" },
-    { NEQ, "!=" },
-    { ARROW, "->" },
-    { INCREMENT, "++" },
-    { DECREMENT, "--" },
-    { LSHIFT, "<<" },
-    { RSHIFT, ">>" },
-    { TOKEN_PASTE, "##" },
-    { '|', "|" },
-    { '&', "&" },
-    { '^', "^" },
-    { '%', "\x25" },
-    { '<', "<" },
-    { '>', ">" },
-    { '(', "(" },
-    { ')', ")" },
-    { ';', ";" },
-    { ':', ":" },
-    { '{', "{" },
-    { '}', "}" },
-    { '[', "[" },
-    { ']', "]" },
-    { ',', "," },
-    { '.', "." },
-    { '=', "=" },
-    { '*', "*" },
-    { '/', "/" },
-    { '+', "+" },
-    { '-', "-" },
-    { '!', "!" },
-    { '?', "?" },
-    { '~', "~" },
-    { '#', "#" }
-};
+/* Macros to make state state machine implementation of identifier and operator
+ * tokenization simpler.
+ */
+#define at(c) (*in == (c) && in++)
+#define get(c) (*in++ == (c))
+#define end() !isident(*in)
+#define ret(t, s) \
+    do { \
+        *endptr = in; \
+        res.token = t; \
+        res.strval = s; \
+        return res; \
+    } while (0)
+
+#define S1(a) (at(a) && end())
+#define S2(a, b) (at(a) && get(b) && end())
+#define S3(a, b, c) (at(a) && get(b) && get(c) && end())
+#define S4(a, b, c, d) (at(a) && get(b) && get(c) && get(d) && end())
+#define S5(a, b, c, d, e) \
+    (at(a) && get(b) && get(c) && get(d) && get(e) && end())
+#define S6(a, b, c, d, e, f) \
+    (at(a) && get(b) && get(c) && get(d) && get(e) && get(f) && end())
+#define S7(a, b, c, d, e, f, g) \
+    (at(a) && get(b) && get(c) && get(d) && get(e) && get(f) && get(g) && end())
+
+/* Parse string as keyword or identifier. First character should be alphabetic
+ * or underscore.
+ */
+static struct token strtoident(char *in, char **endptr)
+{
+    struct token res = {IDENTIFIER};
+
+    switch (*in++) {
+    case 'a':
+        if (S3('u', 't', 'o'))                      ret(AUTO, "auto");
+        break;
+    case 'b':
+        if (S4('r', 'e', 'a', 'k'))                 ret(BREAK, "break");
+        break;
+    case 'c':
+        if (S3('a', 's', 'e'))                      ret(CASE, "case");
+        if (S3('h', 'a', 'r'))                      ret(CHAR, "char");
+        if (at('o') && get('n')) {
+            if (S2('s', 't'))                       ret(CONST, "const");
+            if (S5('t', 'i', 'n', 'u', 'e'))        ret(CONTINUE, "continue");
+        }
+        break;
+    case 'd':
+        if (S6('e', 'f', 'a', 'u', 'l', 't'))       ret(DEFAULT, "default");
+        if (at('o')) {
+            if (S4('u', 'b', 'l', 'e'))             ret(DOUBLE, "double");
+            if (end())                              ret(DO, "do");
+        }
+        break;
+    case 'e':
+        if (S3('l', 's', 'e'))                      ret(ELSE, "else");
+        if (S3('n', 'u', 'm'))                      ret(ENUM, "enum");
+        if (S5('x', 't', 'e', 'r', 'n'))            ret(EXTERN, "extern");
+        break;
+    case 'f':
+        if (S4('l', 'o', 'a', 't'))                 ret(FLOAT, "float");
+        if (S2('o', 'r'))                           ret(FOR, "for");
+        break;
+    case 'g':
+        if (S3('o', 't', 'o'))                      ret(GOTO, "goto");
+        break;
+    case 'i':
+        if (S1('f'))                                ret(IF, "if");
+        if (S2('n', 't'))                           ret(INT, "int");
+        break;
+    case 'l':
+        if (S3('o', 'n', 'g'))                      ret(LONG, "long");
+        break;
+    case 'r':
+        if (at('e')) {
+            if (S6('g', 'i', 's', 't', 'e', 'r'))   ret(REGISTER, "register");
+            if (S4('t', 'u', 'r', 'n'))             ret(RETURN, "return");
+        }
+        break;
+    case 's':
+        if (S4('h', 'o', 'r', 't'))                 ret(SHORT, "short");
+        if (S5('w', 'i', 't', 'c', 'h'))            ret(SWITCH, "switch");
+        if (at('i')) {
+            if (S4('g', 'n', 'e', 'd'))             ret(SIGNED, "signed");
+            if (S4('z', 'e', 'o', 'f'))             ret(SIZEOF, "sizeof");
+        }
+        if (at('t')) {
+            if (S4('a', 't', 'i', 'c'))             ret(STATIC, "static");
+            if (S4('r', 'u', 'c', 't'))             ret(STRUCT, "struct");
+        }
+        break;
+    case 't':
+        if (S6('y', 'p', 'e', 'd', 'e', 'f'))       ret(TYPEDEF, "typedef");
+        break;
+    case 'u':
+        if (at('n')) {
+            if (S3('i', 'o', 'n'))                  ret(UNION, "union");
+            if (S6('s', 'i', 'g', 'n', 'e', 'd'))   ret(UNSIGNED, "unsigned");
+        }
+        break;
+    case 'v':
+        if (at('o')) {
+            if (S2('i', 'd'))                       ret(VOID, "void");
+            if (S6('l', 'a', 't', 'i', 'l', 'e'))   ret(VOLATILE, "volatile");
+        }
+        break;
+    case 'w':
+        if (S4('h', 'i', 'l', 'e'))                 ret(WHILE, "while");
+    default:
+        break;
+    }
+
+    /* Fallthrough means we have consumed at least one character, and the token
+     * should be identifier. Backtrack one position to correct a get() that
+     * moved us past the end. */
+    in--;
+
+    while (isident(*in))
+        in++;
+
+    *endptr = in;
+    return res;
+}
+
+static struct token strtoop(char *in, char **endptr)
+{
+    struct token res = {0};
+
+    switch (*in++) {
+    case '*':
+        if (at('=')) ret(MUL_ASSIGN, "*=");
+        ret('*', "*");
+    case '/':
+        if (at('=')) ret(DIV_ASSIGN, "/=");
+        ret('/', "/");
+    case '%':
+        if (at('=')) ret(MOD_ASSIGN, "\x25=");
+        ret('%', "\x25");
+    case '+':
+        if (at('+')) ret(INCREMENT, "++");
+        if (at('=')) ret(PLUS_ASSIGN, "+=");
+        ret('+', "+");
+    case '-':
+        if (at('>')) ret(ARROW, "->");
+        if (at('-')) ret(DECREMENT, "--");
+        if (at('=')) ret(MINUS_ASSIGN, "-=");
+        ret('-', "-");
+    case '<':
+        if (at('=')) ret(LEQ, "<=");
+        if (at('<')) {
+            if (at('=')) ret(LSHIFT_ASSIGN, "<<=");
+            ret(LSHIFT, "<<");
+        }
+        ret('<', "<");
+    case '>':
+        if (at('=')) ret(GEQ, ">=");
+        if (at('>')) {
+            if (at('=')) ret(RSHIFT_ASSIGN, ">>=");
+            ret(RSHIFT, ">>");
+        }
+        ret('>', ">");
+    case '&':
+        if (at('=')) ret(AND_ASSIGN, "&=");
+        if (at('&')) ret(LOGICAL_AND, "||");
+        ret('&', "&");
+    case '^':
+        if (at('=')) ret(XOR_ASSIGN, "^=");
+        ret('^', "^");
+    case '|':
+        if (at('=')) ret(OR_ASSIGN, "|=");
+        if (at('|')) ret(LOGICAL_OR, "||");
+        ret('|', "|");
+    case '.':
+        if (at('.') && get('.')) ret(DOTS, "...");
+        ret('.', ".");
+    case '=':
+        if (at('=')) ret(EQ, "==");
+        ret('=', "=");
+    case '!':
+        if (at('=')) ret(NEQ, "!=");
+        ret('!', "!");
+    case '#':
+        if (at('#')) ret(TOKEN_PASTE, "##");
+        ret('#', "#");
+    case '(': ret('(', "(");
+    case ')': ret(')', ")");
+    case ';': ret(';', ";");
+    case ':': ret(':', ":");
+    case '{': ret('{', "{");
+    case '}': ret('}', "}");
+    case '[': ret('[', "[");
+    case ']': ret(']', "]");
+    case ',': ret(',', ",");
+    case '?': ret('?', "?");
+    case '~': ret('~', "~");
+    default:
+        break;
+    }
+
+    return res;
+}
 
 struct token
     token_end = { END, "$" },
@@ -254,8 +375,9 @@ struct token tokenize(char *in, char **endptr)
     assert(endptr);
     assert(in);
 
+    *endptr = in;
+
     if (*in == '\0') {
-        *endptr = in;
         return token_end;
     } else if (isspace(*in)) {
         res.token = SPACE;
@@ -264,27 +386,13 @@ struct token tokenize(char *in, char **endptr)
             res.strval = str_register_n(in, *endptr - in);
             return res;
         }
-        error("Invalid whitespace sequence: '%s'.", in);
     } else if (isalpha(*in) || *in == '_') {
-        int n = 0;
-        for (; n < sizeof(keywords) / sizeof(keywords[0]); ++n) {
-            int length = strlen(keywords[n].strval);
-            if (!strncmp(in, keywords[n].strval, length)) {
-                if (isalnum(in[length]) || in[length] == '_') {
-                    /* Reserved word is a prefix of an identifier. */
-                    break;
-                }
-                *endptr = in + length;
-                return keywords[n];
-            }
+        res = strtoident(in, endptr);
+        assert(*endptr != in);
+        if (res.token == IDENTIFIER) {
+            res.strval = str_register_n(in, *endptr - in);
         }
-        res.strval = strtoident(in, endptr);
-        if (*endptr != in) {
-            res.token = IDENTIFIER;
-            res.strval = str_register(res.strval);
-            return res;
-        }
-        error("Invalid identifier: '%s'.", in);
+        return res;
     } else if (isdigit(*in)) {
         res.intval = strtonum(in, endptr);
         if (*endptr != in) {
@@ -292,7 +400,6 @@ struct token tokenize(char *in, char **endptr)
             res.strval = str_register_n(in, *endptr - in);
             return res;
         }
-        error("Invalid number literal: '%s'.", in);
     } else if (*in == '"') {
         res.strval = strtostr(in, endptr);
         if (*endptr != in) {
@@ -300,7 +407,6 @@ struct token tokenize(char *in, char **endptr)
             res.strval = str_register(res.strval);
             return res;
         }
-        error("Invalid string literal: '%s'.", in);
     } else if (*in == '\'') {
         res.intval = strtochar(in, endptr);
         if (*endptr != in) {
@@ -308,17 +414,7 @@ struct token tokenize(char *in, char **endptr)
             res.strval = str_register_n(in, *endptr - in);
             return res;
         }
-        error("Invalid character literal: '%s'.", in);
-    } else {
-        int i = 0;
-        for (; i < sizeof(operators) / sizeof(operators[0]); ++i) {
-            int len = strlen(operators[i].strval);
-            if (!strncmp(in, operators[i].strval, len)) {
-                *endptr = in + len;
-                return operators[i];
-            }
-        }
-        error("Invalid token '%c'.", *in);
     }
-    exit(1);
+
+    return strtoop(in, endptr);
 }
