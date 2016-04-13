e39ed8c755fbd7d8a424d0204220680142d40771
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2016-04-13 20:24:49 +0200

    Store string object and text in the same allocation
    Trick to shave off some calls to memory allocation, storing struct string
    instances and the char arrays pointed to concatenated together.

--- src/preprocessor/strtab.c
+++ src/preprocessor/strtab.c
@@ -9,39 +9,40 @@
 
 static struct hash_table strtab;
 
-static void strtab_free(void)
-{
-    hash_destroy(&strtab);
-}
-
+/* Every unique string encountered, being identifiers or literals, is
+ * kept for the lifetime of the program. To save allocations, store the
+ * raw string buffer in the same allocation as the struct.
+ *
+ *  _____ struct string _____    ________ const char [] ________
+ * |                          | |                               |
+ * [ <ptr to data> | <length> ] [ 'H', 'e', 'l', 'l', 'o', '\0' ]
+ */
 static void *str_hash_add(void *ref)
 {
-    char *buf;
-    struct string
-        *str = (struct string *) ref,
-        *dup;
+    char *buffer;
+    struct string *s;
+    size_t l;
 
-    buf = calloc(str->len + 1, sizeof(*buf));
-    dup = calloc(1, sizeof(*dup));
-
-    dup->str = memcpy(buf, str->str, str->len);
-    dup->len = str->len;
-
-    return dup;
-}
-
-static void str_hash_del(void *ref)
-{
-    struct string *str = (struct string *) ref;
-
-    free((char *) str->str);
-    free(str);
+    s = (struct string *) ref;
+    l = s->len;
+    buffer = malloc(sizeof(struct string) + l + 1);
+    buffer[sizeof(struct string) + l] = '\0';
+    memcpy(buffer + sizeof(struct string), s->str, l);
+    s = (struct string *) buffer;
+    s->str = buffer + sizeof(*s);
+    s->len = l;
+    return s;
 }
 
 static struct string str_hash_key(void *ref)
 {
     struct string *str = (struct string *) ref;
     return *str;
+}
+
+static void strtab_free(void)
+{
+    hash_destroy(&strtab);
 }
 
 struct string str_register(const char *str, size_t len)
@@ -56,7 +57,7 @@ struct string str_register(const char *str, size_t len)
             STRTAB_SIZE,
             str_hash_key,
             str_hash_add,
-            str_hash_del);
+            free);
 
         atexit(strtab_free);
         initialized = 1;
