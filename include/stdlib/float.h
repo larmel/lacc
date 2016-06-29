f962a999876cad738cf92c1ef9a36d8db7dd5b36
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2016-06-29 20:47:20 +0200

    Add float.h implementation header

---
+++ include/stdlib/float.h
@@ -0,0 +1,26 @@
+#ifndef _FLOAT_H
+#define _FLOAT_H
+
+/* IEEE float.
+ */
+#define FLT_RADIX 2
+#define FLT_ROUNDS 1
+#define FLT_DIG 6
+#define FLT_EPSILON 1.19209290e-07f
+#define FLT_MANT_DIG 24
+#define FLT_MAX 3.40282347e+38f
+#define FLT_MAX_EXP 128
+#define FLT_MIN 1.17549435e-38f
+#define FLT_MIN_EXP (-125)
+
+/* IEEE double.
+ */
+#define DBL_DIG 15
+#define DBL_EPSILON 2.2204460492503131e-16
+#define DBL_MANT_DIG 53
+#define DBL_MAX 1.7976931348623157e+308
+#define DBL_MAX_EXP 1024
+#define DBL_MIN 2.2250738585072014e-308
+#define DBL_MIN_EXP (-1021)
+
+#endif
