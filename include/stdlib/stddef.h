736a6c69c423718628204574de9a460641cf3b8a
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2016-07-11 01:10:33 +0200

    Define offsetof macro

--- include/stdlib/stddef.h
+++ include/stdlib/stddef.h
@@ -1,10 +1,11 @@
 #ifndef _STDDEF_H
 #define _STDDEF_H
 
-#define NULL ((void *) 0)
-
 typedef unsigned long size_t;
 typedef signed long ptrdiff_t;
 typedef signed int wchar_t;
+
+#define NULL ((void *) 0)
+#define offsetof(T, field) ((size_t) &((T *) 0)->field)
 
 #endif
