b9e97e2149019f3b8b3df2430e6874a8f7e9ce83
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2016-07-19 22:08:03 +0200

    Handle infinite loops in programs generated by csmith

--- csmith.sh
+++ csmith.sh
@@ -2,22 +2,36 @@
 
 directory="csmith"
 csmith_home="$1"
-csmith_options="--no-packed-struct --max-funcs 1 --max-struct-fields 2 --max-union-fields 2 --max-block-depth 2 --no-jumps"
-if [[ -z "$csmith_home" ]]; then
-	echo "Usage: $0 <csmith home path>"; exit
+csmith_options="--no-packed-struct"
+if [[ -z "$csmith_home" ]]
+then
+	echo "Usage: $0 <csmith home path>"
+	exit
 fi
 
 n=1
 while [ true ]
 do
 	filename="${directory}/${n}.c"
-	${csmith_home}/src/csmith ${csmith_options} > ${filename};
+	program="${directory}/${n}"
+	${csmith_home}/src/csmith ${csmith_options} > ${filename}
+
+	cc -w -std=c99 -I ${csmith_home}/runtime ${filename} -o ${program}
+	timeout 1 ${program} > /dev/null
+	if [ $? -ne 0 ]
+	then
+		rm ${program}
+		continue
+	fi
+
+	rm ${program}
 	./check.sh \
 		"bin/lacc -w -std=c99 -I ${csmith_home}/runtime" ${filename} \
-		"cc -w -std=c99 -I ${csmith_home}/runtime";
-	result="$?"
-	if [[ $result -ne "0" ]]; then
+		"cc -w -std=c99 -I ${csmith_home}/runtime"
+	if [ $? -ne 0 ]
+	then
 		break
 	fi
+
 	n=$((n + 1))
 done
