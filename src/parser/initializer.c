cf89297117f8f8040e078f8cfed57cca8d104c26
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2017-08-02 20:38:41 +0200

    Handle zero initialization and padding after parsing

--- src/parser/initializer.c
+++ src/parser/initializer.c
@@ -8,109 +8,11 @@
 
 #include <assert.h>
 
-static const struct var var__immediate_zero = {IMMEDIATE, {T_INT}};
-
 static struct block *initialize_member(
     struct definition *def,
     struct block *block,
     struct block *values,
     struct var target);
-
-static int is_string(struct expression expr)
-{
-    return is_identity(expr)
-        && expr.l.kind == IMMEDIATE && expr.l.symbol
-        && expr.l.symbol->symtype == SYM_STRING_VALUE;
-}
-
-/*
- * Set var = 0, using simple assignment on members for composite types.
- *
- * This rule does not consume any input, but generates a series of
- * assignments on the given variable. Point is to be able to zero
- * initialize using normal simple assignment rules, although IR can
- * become verbose for large structures.
- */
-static void zero_initialize(
-    struct definition *def,
-    struct block *values,
-    struct var target)
-{
-    int i;
-    size_t size;
-    struct var var;
-
-    assert(target.kind == DIRECT);
-    size = size_of(target.type);
-    switch (type_of(target.type)) {
-    case T_STRUCT:
-    case T_UNION:
-        assert(size);
-        target.type = (size % 8)
-            ? type_create(T_ARRAY, basic_type__char, size, NULL)
-            : type_create(T_ARRAY, basic_type__long, size / 8, NULL);
-    case T_ARRAY:
-        var = target;
-        target.type = type_next(target.type);
-        for (i = 0; i < size / size_of(target.type); ++i) {
-            target.offset = var.offset + i * size_of(target.type);
-            zero_initialize(def, values, target);
-        }
-        break;
-    case T_CHAR:
-    case T_SHORT:
-    case T_INT:
-    case T_LONG:
-    case T_FLOAT:
-    case T_DOUBLE:
-    case T_LDOUBLE:
-    case T_POINTER:
-        var = var__immediate_zero;
-        var.type = target.type;
-        eval_assign(def, values, target, as_expr(var));
-        break;
-    default:
-        error("Cannot zero-initialize object of type '%t'.", target.type);
-        exit(1);
-    }
-}
-
-static void zero_initialize_bytes(
-    struct definition *def,
-    struct block *values,
-    struct var target,
-    size_t bytes)
-{
-    size_t size;
-    target.field_offset = 0;
-    target.field_width = 0;
-
-    while (bytes) {
-        size = bytes % 8;
-        if (!size) {
-            size = 8;
-        }
-        assert(size <= bytes);
-        switch (size) {
-        default:
-            target.type = basic_type__char;
-            break;
-        case 2:
-            target.type = basic_type__short;
-            break;
-        case 4:
-            target.type = basic_type__int;
-            break;
-        case 8:
-            target.type = basic_type__long;
-            break;
-        }
-        zero_initialize(def, values, target);
-        target.offset += size_of(target.type);
-        bytes -= size;
-    }
-}
-
 
 static struct block *read_initializer_element(
     struct definition *def,
@@ -149,73 +51,6 @@ static int next_element(void)
     }
 
     return 0;
-}
-
-/*
- * Zero initialize padding bytes between previous and current member.
- *
- * Offset and field offset written so far is stored in target.
- */
-static void initialize_padding(
-    struct definition *def,
-    struct block *block,
-    struct var target,
-    const struct member *prev,
-    const struct member *member)
-{
-    size_t padding;
-
-    if (!prev) {
-        if (member->field_offset) {
-            target.field_offset = 0;
-            target.field_width = member->field_offset;
-            target.type = basic_type__int;
-            zero_initialize(def, block, target);
-        }
-    } else if (prev->offset == member->offset) {
-        if (prev->field_offset + prev->field_width < member->field_offset) {
-            target.field_offset = prev->field_offset + prev->field_width;
-            target.field_width = member->field_offset - target.field_offset;
-            target.type = basic_type__int;
-            zero_initialize(def, block, target);
-        }
-    } else if (prev->offset + size_of(prev->type) < member->offset) {
-        padding = member->offset - (prev->offset + size_of(prev->type));
-        target.offset += size_of(prev->type);
-        zero_initialize_bytes(def, block, target, padding);
-    }
-}
-
-/*
- * Initialize padding at the end of a struct.
- *
- * Consider both last bits of a bitfield, and any remaining space after
- * the bitfield itself.
- */
-static void initialize_trailing_padding(
-    struct definition *def,
-    struct block *block,
-    struct var target,
-    Type type)
-{
-    size_t padding;
-    const struct member *member;
-
-    member = get_member(type, nmembers(type) - 1);
-    if (member->field_width
-        && member->field_offset + member->field_width < 32)
-    {
-        target.type = basic_type__int;
-        target.field_offset = member->field_offset + member->field_width;
-        target.field_width = 32 - target.field_offset;
-        zero_initialize(def, block, target);
-    }
-
-    if (member->offset + size_of(member->type) < size_of(type)) {
-        padding = size_of(type) - (member->offset + size_of(member->type));
-        target.offset += size_of(member->type);
-        zero_initialize_bytes(def, block, target, padding);
-    }
 }
 
 /*
@@ -264,7 +99,6 @@ static struct block *initialize_struct_or_union(
                     || prev->field_offset != member->field_offset)
                     break;
             }
-            initialize_padding(def, values, target, prev, member);
             target.type = member->type;
             prev = member;
             target.offset = filled + member->offset;
@@ -272,22 +106,16 @@ static struct block *initialize_struct_or_union(
             target.field_width = member->field_width;
             block = initialize_member(def, block, values, target);
         } while (i < m && next_element());
-
-        while (i < m) {
-            member = get_member(type, i++);
-            initialize_padding(def, values, target, prev, member);
-            target.type = member->type;
-            prev = member;
-            target.offset = filled + member->offset;
-            target.field_offset = member->field_offset;
-            target.field_width = member->field_width;
-            zero_initialize(def, values, target);
-        }
-
-        initialize_trailing_padding(def, values, target, type);
     }
 
     return block;
+}
+
+static int is_string(struct expression expr)
+{
+    return is_identity(expr)
+        && expr.l.kind == IMMEDIATE && expr.l.symbol
+        && expr.l.symbol->symtype == SYM_STRING_VALUE;
 }
 
 /*
@@ -303,7 +131,8 @@ static struct block *initialize_struct_or_union(
  *
  *      char foo[5] = "Hi"
  *
- * This will generates the following IR assignments:
+ * This will generates the following IR assignments, after padding is
+ * added at the end:
  *
  *      foo = "Hi"
  *      foo[3] = 0
@@ -316,7 +145,6 @@ static struct block *initialize_array(
     struct var target)
 {
     Type type = target.type;
-    size_t filled = target.offset;
 
     assert(is_array(type));
     assert(target.kind == DIRECT);
@@ -346,18 +174,11 @@ static struct block *initialize_array(
         assert(is_array(target.symbol->type));
         assert(!size_of(target.symbol->type));
         type_set_array_size(target.symbol->type, target.offset);
-    } else {
-        target.type = type_next(type);
-        while (target.offset - filled < size_of(type)) {
-            zero_initialize(def, values, target);
-            target.offset += size_of(target.type);
-        }
     }
 
     return block;
 }
 
-/* Initialize member of an aggregate type. */
 static struct block *initialize_member(
     struct definition *def,
     struct block *block,
@@ -428,16 +249,240 @@ static struct block *initialize_object(
     return block;
 }
 
+static const struct var var__immediate_zero = {IMMEDIATE, {T_INT}};
+
+/*
+ * Set var = 0, using simple assignment on members for composite types.
+ *
+ * This rule does not consume any input, but generates a series of
+ * assignments on the given variable. Point is to be able to zero
+ * initialize using normal simple assignment rules, although IR can
+ * become verbose for large structures.
+ */
+static void zero_initialize(
+    struct definition *def,
+    struct block *values,
+    struct var target)
+{
+    int i;
+    size_t size;
+    struct var var;
+
+    assert(target.kind == DIRECT);
+    size = size_of(target.type);
+    switch (type_of(target.type)) {
+    case T_STRUCT:
+    case T_UNION:
+        assert(size);
+        target.type = (size % 8)
+            ? type_create(T_ARRAY, basic_type__char, size, NULL)
+            : type_create(T_ARRAY, basic_type__long, size / 8, NULL);
+    case T_ARRAY:
+        var = target;
+        target.type = type_next(target.type);
+        for (i = 0; i < size / size_of(target.type); ++i) {
+            target.offset = var.offset + i * size_of(target.type);
+            zero_initialize(def, values, target);
+        }
+        break;
+    case T_CHAR:
+    case T_SHORT:
+    case T_INT:
+    case T_LONG:
+    case T_FLOAT:
+    case T_DOUBLE:
+    case T_LDOUBLE:
+    case T_POINTER:
+        var = var__immediate_zero;
+        var.type = target.type;
+        eval_assign(def, values, target, as_expr(var));
+        break;
+    default:
+        error("Cannot zero-initialize object of type '%t'.", target.type);
+        exit(1);
+    }
+}
+
+static void zero_initialize_bytes(
+    struct definition *def,
+    struct block *values,
+    struct var target,
+    size_t bytes)
+{
+    size_t size;
+
+    target.field_offset = 0;
+    target.field_width = 0;
+    while (bytes) {
+        size = bytes % 8;
+        if (!size) {
+            size = 8;
+        }
+        assert(size <= bytes);
+        switch (size) {
+        default:
+            size = 1;
+        case 1:
+            target.type = basic_type__char;
+            break;
+        case 2:
+            target.type = basic_type__short;
+            break;
+        case 4:
+            target.type = basic_type__int;
+            break;
+        case 8:
+            target.type = basic_type__long;
+            break;
+        }
+        zero_initialize(def, values, target);
+        target.offset += size_of(target.type);
+        bytes -= size;
+    }
+}
+
+/*
+ * Zero initialize padding bytes between target reference and next field
+ * assignment.
+ *
+ * Target has offset and field offset pointing to first location not yet
+ * initialized.
+ */
+static void initialize_padding(
+    struct definition *def,
+    struct block *block,
+    struct var target,
+    struct var field)
+{
+    size_t padding;
+
+    if (target.offset < field.offset) {
+        if (target.field_offset) {
+            target.type = basic_type__int;
+            target.field_width = 32 - target.field_offset;
+            zero_initialize(def, block, target);
+            target.offset += size_of(target.type);
+        }
+        padding = field.offset - target.offset;
+        zero_initialize_bytes(def, block, target, padding);
+    } else if (target.field_offset < field.field_offset) {
+        target.type = basic_type__int;
+        target.field_width = field.field_offset - target.field_offset;
+        zero_initialize(def, block, target);
+    }
+}
+
+/*
+ * Initialize padding at the end of object.
+ *
+ * Consider both last bits of a bitfield, and any remaining space after
+ * the bitfield itself.
+ */
+static void initialize_trailing_padding(
+    struct definition *def,
+    struct block *block,
+    struct var target,
+    size_t size)
+{
+    if (target.field_offset) {
+        target.type = basic_type__int;
+        target.field_width = 32 - target.field_offset;
+        zero_initialize(def, block, target);
+        target.offset += size_of(target.type);
+    }
+
+    if (size > target.offset) {
+        zero_initialize_bytes(def, block, target, size - target.offset);
+    }
+}
+
 /*
  * Evaluate sequence of IR_ASSIGN statements in a separate block. This
  * is appended at the end after all expressions inside initializers are
  * evaluated.
+ *
+ * Padding initialization is handled only after the whole initializer is
+ * read, as postprocessing of the statements in this block.
  */
-static struct block *get_initializer_block()
+static struct block *get_initializer_block(int i)
 {
-    static struct block *block;
+    static struct block *block[2];
 
-    return !block ? (block = cfg_block_init(NULL)) : block;
+    return !block[i] ? (block[i] = cfg_block_init(NULL)) : block[i];
+}
+
+#ifndef NDEBUG
+static void validate_initializer_block(struct block *block)
+{
+    int i;
+    struct statement st;
+    struct var target = {0}, field;
+
+    for (i = 0; i < array_len(&block->code); ++i) {
+        st = array_get(&block->code, i);
+        assert(st.st == IR_ASSIGN);
+        assert(target.offset <= st.t.offset);
+        field = st.t;
+        if (target.offset < field.offset) {
+            assert(field.offset - target.offset == size_of(target.type));
+        } else {
+            assert(target.offset == field.offset);
+            assert(target.field_offset + target.field_width
+                == field.field_offset);
+        }
+
+        target = field;
+    }
+}
+#endif
+
+/*
+ * Fill in any missing padding initialization in assignment statment
+ * list.
+ *
+ * The input block contains a list of assignments to the same variable,
+ * possibly sparsely covering the full size of the type.
+ */
+static struct block *postprocess_object_initialization(
+    struct definition *def,
+    struct block *values,
+    struct var target)
+{
+    int i;
+    struct statement st;
+    struct var field;
+    struct block *block = get_initializer_block(1);
+
+    for (i = 0; i < array_len(&values->code); ++i) {
+        st = array_get(&values->code, i);
+        assert(st.st == IR_ASSIGN);
+        assert(target.offset <= st.t.offset);
+        field = st.t;
+
+        initialize_padding(def, block, target, field);
+
+        array_push_back(&block->code, st);
+        target.offset = field.offset;
+        if (field.field_width) {
+            target.field_offset = field.field_offset + field.field_width;
+            if (target.field_offset == 32) {
+                target.field_offset = 0;
+                target.field_width = 0;
+                target.offset += size_of(field.type);
+            }
+        } else {
+            target.field_offset = 0;
+            target.field_width = 0;
+            target.offset += size_of(field.type);
+        }
+    }
+
+    initialize_trailing_padding(def, block, target, size_of(target.type));
+    array_empty(&values->code);
+#ifndef NDEBUG
+    validate_initializer_block(block);
+#endif
+    return block;
 }
 
 struct block *initializer(
@@ -445,11 +490,12 @@ struct block *initializer(
     struct block *block,
     const struct symbol *sym)
 {
-    struct block *values = get_initializer_block();
+    struct block *values = get_initializer_block(0);
     struct var target = var_direct(sym);
 
     if (peek().token == '{' || is_array(sym->type)) {
         block = initialize_object(def, block, values, target);
+        values = postprocess_object_initialization(def, values, target);
         array_concat(&block->code, &values->code);
         array_empty(&values->code);
     } else {
