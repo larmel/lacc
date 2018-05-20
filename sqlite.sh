f88ba2df5e6bf6e64eb4535158fd38bd0b5253de
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2018-05-20 02:48:51 -0700

    Rewrite makefile to be more simple and standard compliant
    Should now be possible to use implementations other than gmake.
    Also improve validation of selfhosting, by doing binary comparison
    on the object files.

--- sqlite.sh
+++ sqlite.sh
@@ -1,14 +1,13 @@
-#!/bin/bash
+#!/bin/sh
 
-lacc="$1"
-comp="$2"
-if [[ -z "$comp" ]]
+comp=$1
+if [ -z "$comp" ] || [ ! -f "$comp" ]
 then
-	echo "Usage: $0 <compiler to test> <reference compiler>";
+	echo "Usage: $0 <compiler>";
 	exit 1
 fi
 
-if [[ ! -f sqlite/shell.c || ! -f sqlite/sqlite3.c ]]
+if [ ! -f sqlite/shell.c ] || [ ! -f sqlite/sqlite3.c ]
 then
 	echo "Missing sqlite source, download and place in 'sqlite' folder"
 	exit 1
@@ -17,12 +16,12 @@ fi
 flags="-DSQLITE_DEBUG -DSQLITE_MEMDEBUG -c -v --dump-symbols --dump-types"
 
 # Build with lacc
-$lacc -c sqlite/shell.c -o bin/shell.o
-valgrind $lacc $flags sqlite/sqlite3.c -o bin/sqlite3.o > /dev/null
-$comp bin/shell.o bin/sqlite3.o -o bin/sqlite -lm -lpthread -ldl
+$comp -fPIC -c sqlite/shell.c -o bin/shell.o
+valgrind $comp -fPIC $flags sqlite/sqlite3.c -o bin/sqlite3.o > /dev/null
+cc bin/shell.o bin/sqlite3.o -o bin/sqlite -lm -lpthread -ldl
 
 # Build with reference compiler
-$comp sqlite/shell.c sqlite/sqlite3.c -o bin/sqlite-cc -lm -lpthread -ldl
+cc sqlite/shell.c sqlite/sqlite3.c -o bin/sqlite-cc -lm -lpthread -ldl
 
 # Test case
 input=$(cat <<EOF
@@ -36,11 +35,11 @@ EOF
 expected=$(echo "$input" | bin/sqlite-cc)
 actual=$(echo "$input" | bin/sqlite)
 
-if [[ "$expected" != "$actual" ]]
+if [ "$expected" != "$actual" ]
 then
-	echo "$(tput setaf 1)Wrong output!$(tput sgr 0)";
+	echo "$(tput setaf 1)Wrong output!$(tput sgr0)";
 	exit 1
 fi
 
-echo "$(tput setaf 2)Ok!$(tput sgr 0)"
+echo "$(tput setaf 2)Ok!$(tput sgr0)"
 exit 0
