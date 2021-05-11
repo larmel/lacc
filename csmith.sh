287e3577d69fcb5da1c373ab69921e5e856f8c77
Lars Kirkholt Melhus <lars.kirkholt.melhus@gmail.com>
2021-05-11 10:17:42 +0200

    Verify diffs with clang before reporting bug from csmith

--- csmith.sh
+++ csmith.sh
@@ -2,8 +2,8 @@
 
 include="/usr/include/csmith"
 
-command -v lacc >/dev/null 2>&1 || {
-	echo "lacc required, run 'make install'"
+command -v bin/lacc >/dev/null 2>&1 || {
+	echo "bin/lacc required, run 'make'"
 	exit 1
 }
 
@@ -23,18 +23,37 @@ do
 
 	gcc -w -std=c99 -I "$include" "$filename" -o "$program"
 	if [ $? -ne 0 ]; then
+		echo "Failed to compile with gcc"
 		exit 1
 	fi
 
-	timeout 1 "$program" > /dev/null
+	timeout 1 "$program" > "csmith/gcc.txt"
 	if [ $? -ne 0 ]; then
 		rm "$program"
 		continue
 	fi
 
+	clang -w -std=c99 -I "$include" "$filename" -o "$program"
+	if [ $? -ne 0 ]; then
+		echo "Failed to compile with clang"
+		exit 1
+	fi
+
+	timeout 1 "$program" > "csmith/clang.txt"
+	if [ $? -ne 0 ]; then
+		echo "Failed to run with clang"
+		continue
+	fi
+
+	diff "csmith/gcc.txt" "csmith/clang.txt"
+	if [ $? -ne 0 ]; then
+		echo "Skipping test because gcc and clang differ"
+		continue
+	fi
+
 	rm "$program"
 	./check.sh \
-		"lacc -w -std=c99 -I ${include}" "$filename" \
+		"bin/lacc -w -std=c99 -I ${include}" "$filename" \
 		"gcc -w -std=c99 -I ${include}"
 	if [ $? -ne 0 ]; then
 		break
@@ -50,14 +69,14 @@ done
 # checksum calculation of other variables, and fix lines that produce
 # warnings.
 #
-# ./check.sh "lacc -std=c99" creduce/test.c "gcc -std=c99" 2> foo.out
+# ./check.sh "bin/lacc -std=c99" creduce/test.c "cc -std=c99" 2> foo.out
 #
 # Modify creduce/interesting.sh to contain the new values for expected
 # and actual checksum, provided as input script to creduce.
 
 mkdir -p creduce
-lacc -std=c99 -I "$include" -w -E "$filename" -o creduce/test.c
-lacc -std=c99 -I "$include" -w "$filename" -o creduce/test -lm
+bin/lacc -std=c99 -I "$include" -w -E "$filename" -o creduce/test.c
+bin/lacc -std=c99 -I "$include" -w "$filename" -o creduce/test -lm
 gcc -std=c99 -I "$include" "$filename" -o creduce/test-cc
 cp interesting.sh creduce
 creduce/test 1 > creduce/lacc.out && creduce/test-cc 1 > creduce/cc.out
